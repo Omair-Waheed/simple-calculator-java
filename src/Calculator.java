25d5a9062275793dcd8256e71982af3a5d2336df
Omair Waheed <waheedomair@gmail.com>
2025-11-15 19:27:56 -0800

    Initialize project in VS Code
    Signed-off-by: Omair Waheed <waheedomair@gmail.com>

---
+++ src/Calculator.java
@@ -0,0 +1,42 @@
+/*
+Copyright (C) 2025 Omair Waheed
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+//
+//
+//Small program to show git usage.
+//Calculator functions taken from features section this page: https://www.hp.com/us-en/shop/tech-takes/what-is-a-scientific-calculator
+//Will include: addition, subtraction, multiplication, division, 
+//trignometric (sin, cos, tan, csc, sec, cot, inverses) functions, and 
+//exponential and logarithmic funtions.
+//
+//
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		System.out.print("Java Calculator.");
+	}//main
+	
+	
+	
+}//Calculator
+
+/* 
+
+
+
+*/
